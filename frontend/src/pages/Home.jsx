f0950df269ac547c41e30cfda3b187bee091876e
Aylin Noiret <aylinndream@hotmail.fr>
2023-07-19 16:29:08 +0200

    Suite de la mise en forme

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import logo from "../assets/logo.png";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
           <div className="notebook-front">
             <div className="notebook-skin"> Daily Purgatory </div>
           </div>
+          <img className="logo-carnet" src={logo} alt="" />
           <div className="notebook-bullet-page dotted-page" />
         </div>
       </div>
